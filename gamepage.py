7ffc1efc6b5f2c1a17e87a17173151ce798aaefc
Richard Gibson <richard.g.gibson@gmail.com>
2013-09-26 12:11:32 -0700

    Removed database query in gamepage

--- gamepage.py
+++ gamepage.py
@@ -10,17 +10,14 @@ class GamePage( handler.Handler ):
         # Set this page to be the return page after a login/logout/signup
         self.set_return_url( '/game/' + game_code )
 
-        # Make sure this game has a run
-        q = db.Query( runs.Runs, keys_only=True )
-        q.filter( 'game_code =', game_code )
-        run = q.get( )
-        if not run:
+        # Make sure this game exists
+        game = self.get_game( game_code )
+        if not game:
             self.error( 404 )
             self.render( "404.html", user=user )
             return
 
         rundict = self.get_rundict( game_code )
         
-        self.render( "gamepage.html", user=user, 
-                     game=self.get_game( game_code ), 
+        self.render( "gamepage.html", user=user, game=game, 
                      game_code=game_code, rundict=rundict )
