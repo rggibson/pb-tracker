1b60a70b0830d3b72babe120d57d3d3281f973eb
Richard Gibson <richard.g.gibson@gmail.com>
2013-10-21 16:17:03 -0700

    Using lazy loading now for almost all images

--- static/js/util.js
+++ static/js/util.js
@@ -1,28 +1,3 @@
-function set_boxart_if_exists( img_id, game_code ) {
-    var img_url = "/static/boxart/" + game_code + ".jpg";
-    $.ajax( {
-	url: img_url,
-	type: "HEAD",
-	success: function() {
-	    var elem = document.getElementById( img_id );
-	    elem.setAttribute( "src", img_url ); 
-	}
-    } );
-}
-
-function set_img( img_id, img_src ) {
-    if( img_src ) {
-	$.ajax( {
-	    url: img_src,
-	    type: "HEAD",
-	    success: function() {
-		var elem = document.getElementById( img_id );
-		elem.setAttribute( "src", img_src ); 
-	    }
-	} );
-    }
-}
-
 function block_double_submit( button_id ) {
     $( 'form' ).on( 'submit', function( ) {
 	$( button_id ).button( 'loading' )
