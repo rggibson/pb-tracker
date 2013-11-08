5c3ea5970ef655d5a346b5881759863adf2f9810
Richard Gibson <richard.g.gibson@gmail.com>
2013-11-07 16:58:15 -0800

    Updated front page and faq with Time Split Tracker auto-submission info

--- fixerupper.py
+++ fixerupper.py
@@ -14,6 +14,7 @@ import games
 import runs
 import util
 import handler
+import json
 
 from google.appengine.ext import db
 
@@ -26,24 +27,48 @@ class FixerUpper( handler.Handler ):
             self.render( "404.html", user=user )
             return
 
-        # Recalculate num_pbs for every game
-        q = db.Query( games.Games )
-        q.ancestor( games.key() )
-        for game_model in q.run( limit=100000 ):
-            q2 = db.Query( runs.Runs, projection=('username', 'category'),
-                           distinct=True )
-            q2.ancestor( runs.key() )
-            q2.filter( 'game =', game_model.game )
-            num_pbs = q2.count( limit=1000 )
-            if game_model.num_pbs != num_pbs:
-                self.write( game_model.game + ": " + str( game_model.num_pbs )
-                            + " -> " + str( num_pbs ) + "<br>" )
-                game_model.num_pbs = num_pbs
+        # Mark all of the games that were originally uploaded
+        f = open( 'json/import_games.json', 'r' )
+        iglist = json.loads( f.read( ) )
+        f.close( )
+
+        for d in iglist:
+            game_code = util.get_code( d['game'] )
+            game_model = self.get_game_model( game_code )
+            if game_model is None:
+                self.write( 'No game model for game ' + d['game'] + '<br>' )
+                continue
+            gameinfolist = json.loads( game_model.info )
+            modified = False
+            for gameinfo in gameinfolist:
+                category = gameinfo.get( 'category' )
+                if category in d['categories']:
+                    gameinfo['is_base_category'] = True
+                    modified = True
+                    self.write( d['game'] + ' - ' + category + '<br>' )
+            if modified:
+                game_model.info = json.dumps( gameinfolist )
                 game_model.put( )
-                # Update memcache
-                self.update_cache_game_model( util.get_code( game_model.game ),
-                                              game_model )
-                self.update_cache_gamelist( None )
+                self.update_cache_game_model( game_code, game_model )
+
+        # # Recalculate num_pbs for every game
+        # q = db.Query( games.Games )
+        # q.ancestor( games.key() )
+        # for game_model in q.run( limit=100000 ):
+        #     q2 = db.Query( runs.Runs, projection=('username', 'category'),
+        #                    distinct=True )
+        #     q2.ancestor( runs.key() )
+        #     q2.filter( 'game =', game_model.game )
+        #     num_pbs = q2.count( limit=1000 )
+        #     if game_model.num_pbs != num_pbs:
+        #         self.write( game_model.game + ": " + str( game_model.num_pbs )
+        #                     + " -> " + str( num_pbs ) + "<br>" )
+        #         game_model.num_pbs = num_pbs
+        #         game_model.put( )
+        #         # Update memcache
+        #         self.update_cache_game_model( util.get_code( game_model.game ),
+        #                                       game_model )
+        #         self.update_cache_gamelist( None )
 
         # # Recalculate num_pbs for every runner
         # q = db.Query( runners.Runners )
@@ -63,27 +88,5 @@ class FixerUpper( handler.Handler ):
         #         self.update_cache_runner( util.get_code( runner.username ),
         #                                   runner )
         #         self.update_cache_runnerlist( None )
-
-        # # Fix all the two-digit dates
-        # q = db.Query( runs.Runs )
-        # q.ancestor( runs.key() )
-        # q.order( 'date' )
-        # for run in q.run( limit=100000 ):
-        #     if run.date.year >= 1970:
-        #         break
-        #     if run.date.year >= 0 and run.date.year <= 13:
-        #         self.write( "Replacing year " + str( run.date.year ) 
-        #                     + " for run with id " 
-        #                     + str( run.key().id() ) + "<br>" )
-        #         run.date = run.date.replace( year=(run.date.year + 2000) )
-        #         run.put( )
-        #     elif run.date.year >= 70 and run.date.year < 100:
-        #         self.write( "Replacing year for run with id " 
-        #                     + str( run.key().id() ) + "<br>" )
-        #         run.date = run.date.replace( year=(run.date.year + 1900) )
-        #         run.put( )
-        #     else:
-        #         self.write( "Run with id " + str( run.key().id() ) 
-        #                     + " has bad year " + str( run.date.year ) + "<br>" )
 
         self.write( "FixerUpper complete!<br>" )
