79789401cda8f7c0400e622575441780d9e80d80
Richard Gibson <richard.g.gibson@gmail.com>
2013-11-18 15:12:51 -0800

    Now manually sorting date on listruns page to fix apparent bug

--- static/js/listruns.js
+++ static/js/listruns.js
@@ -20,13 +20,67 @@ function timestr_to_secs( timestr ) {
 }
 
 jQuery.fn.dataTableExt.oSort['time-asc'] = function( x, y ) {
-    x_secs = timestr_to_secs( x );
-    y_secs = timestr_to_secs( y );
+    var x_secs = timestr_to_secs( x );
+    var y_secs = timestr_to_secs( y );
     return ( ( x_secs < y_secs ) ? -1 : ( ( x_secs > y_secs ) ? 1 : 0 ) );
 };
 
 jQuery.fn.dataTableExt.oSort['time-desc'] = function( x, y ) {
-    x_secs = timestr_to_secs( x );
-    y_secs = timestr_to_secs( y );
+    var x_secs = timestr_to_secs( x );
+    var y_secs = timestr_to_secs( y );
     return ( ( x_secs < y_secs ) ? 1 : ( ( x_secs > y_secs ) ? -1 : 0 ) );
+};
+
+var month_to_int = { 
+    "Jan": 0,
+    "Feb": 1,
+    "Mar": 2,
+    "Apr": 3,
+    "May": 4,
+    "Jun": 5,
+    "Jul": 6,
+    "Aug": 7,
+    "Sep": 8,
+    "Oct": 9,
+    "Nov": 10,
+    "Dec": 11 };
+
+jQuery.fn.dataTableExt.oSort['mydate-asc'] = function( x, y ) {
+    var x_parts = x.split( " " );
+    var y_parts = y.split( " " );
+    if( x_parts.length < 4 ) {
+	return ( ( y_parts.length < 4 ) ? 0 : -1 );
+    } else if( y_parts.length < 4 ) {
+	return 1;
+    }
+
+    var x_date = [ x_parts[ 3 ], month_to_int[ x_parts[ 1 ] ], x_parts[ 2 ] ];
+    var y_date = [ y_parts[ 3 ], month_to_int[ y_parts[ 1 ] ], y_parts[ 2 ] ];
+    for( var i = 0; i < x_date.length; ++i ) {
+	if( x_date[ i ] != y_date[ i ] ) {
+	    return ( ( x_date[ i ] < y_date[ i ] ) ? -1 : 1 );
+	}
+    }
+
+    return 0;
+};
+
+jQuery.fn.dataTableExt.oSort['mydate-desc'] = function( x, y ) {
+    var x_parts = x.split( " " );
+    var y_parts = y.split( " " );
+    if( x_parts.length < 4 ) {
+	return ( ( y_parts.length < 4 ) ? 0 : 1 );
+    } else if( y_parts.length < 4 ) {
+	return -1;
+    }
+
+    var x_date = [ x_parts[ 3 ], month_to_int[ x_parts[ 1 ] ], x_parts[ 2 ] ];
+    var y_date = [ y_parts[ 3 ], month_to_int[ y_parts[ 1 ] ], y_parts[ 2 ] ];
+    for( var i = 0; i < x_date.length; ++i ) {
+	if( x_date[ i ] != y_date[ i ] ) {
+	    return ( ( x_date[ i ] < y_date[ i ] ) ? 1 : -1 );
+	}
+    }
+
+    return 0;
 };
