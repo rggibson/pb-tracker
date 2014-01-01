14c69acabd8cbe3743a2f7cfaa3b09cb8f1ad9bb
Richard Gibson <richard.g.gibson@gmail.com>
2014-01-01 01:55:09 -0800

    Restored site datastore writes and finished adding fractions of seconds to run times

--- runs.py
+++ runs.py
@@ -10,11 +10,11 @@ from datetime import date
 def key( name = 'default' ):
     return db.Key.from_path( 'runs', name )    
 
-class Runs( db.Expando ):
+class Runs( db.Model ):
     username = db.StringProperty( required = True )
     game = db.StringProperty( required = True )
     category = db.StringProperty( required = True )
-#    seconds = db.FloatProperty( required = True )
+    seconds = db.FloatProperty( required = True )
     date = db.DateProperty( required = False )
     datetime_created = db.DateTimeProperty( required = True, 
                                             auto_now_add = True )
